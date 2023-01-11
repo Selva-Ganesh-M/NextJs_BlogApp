a068181cfd22973ad692f20a91c4bfdd4654d24f
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-11 19:44:14 +0530

    merged the sanity and next app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div className="underline text-red-600 text-4xl">page</div>;
+};
+
+export default page;
---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ct5tluf2',
+    dataset: 'production'
+  }
+})
