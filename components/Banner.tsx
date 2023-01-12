e914951633b2aa47591a1939c56947ffe9745d72
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-12 18:07:52 +0530

    banner prep

---
+++ components/Banner.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+type Props = {}
+
+const Banner = (props: Props) => {
+  return (
+    <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10'>
+        <div>
+            <h1 className="text-7xl">Selva's Daily blog</h1>
+            <h2 className="mt-5 md:mt-0">
+                Welcome to {""}
+                <span className="underline decoration-4 decoration-[#F7AB0A]">
+                    favourite blog among the devs.
+                </span>
+            </h2>
+        </div>
+
+        <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
+            New product freatures | The latest in technology | The weekly debugging nightmares and more!
+        </p>
+    </div>
+  )
+}
+
+export default Banner
