7b05d0fd4ab0ca2bdd5cecbe91afcd97baf747a2
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-12 16:30:15 +0530

    done with sanity config

---
+++ theme.ts
@@ -0,0 +1,12 @@
+import { buildLegacyTheme } from "sanity";
+
+const props = {
+    "--my-white": "$fff",
+    "--my-black": "$1a1a1a",
+    "--papafam-brand": "$fF7AB0A",
+    "--my-red": "$db4437",
+    "--my-yellow": "$f4b400",
+    "--my-green": "$0f9d58",
+}
+
+
